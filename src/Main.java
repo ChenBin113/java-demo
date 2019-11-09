3185cead11e8444ab0625e05675c74dfb88ffe5e
ChenBin113 <m13680309305_1@163.com>
2019-11-09 23:06:58 +0800

    Add base part.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("-------------- 实验 1 --------------");
+        int i = 0;
+        i = i++;
+        i++;
+        System.out.println(i);
     }
 }
