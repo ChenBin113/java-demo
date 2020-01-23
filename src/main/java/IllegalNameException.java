bde12f286b51b9d069cb316771c233f2c6a9016e
ChenBin113 <m13680309305_1@163.com>
2020-01-23 23:55:52 +0800

    Auto commit.

---
+++ src/main/java/IllegalNameException.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+
+public class IllegalNameException extends Exception {
+
+
+
+    public IllegalNameException() {
+    }
+
+    public IllegalNameException(String message) {
+        super(message);
+    }
+}
